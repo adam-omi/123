65069fc2f715f515ae0f60cd6d4c3eeb75e2c025
adam-omi <adam.omiecinski@gmail.com>
2026-06-14 10:44:22 +0200

    Delete src directory

--- src/main/java/pl/dmcs/rkotas/springbootjsp_iwa2026/model/RoleName.java
+++
@@ -1,8 +0,0 @@
-package pl.dmcs.rkotas.springbootjsp_iwa2026.model;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
-
-
--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
--- to insert only if table is empty
-INSERT INTO role (name) SELECT 'ROLE_ADMIN' WHERE NOT EXISTS (SELECT * FROM role WHERE role.name='ROLE_ADMIN');
-INSERT INTO role (name) SELECT 'ROLE_USER' WHERE NOT EXISTS (SELECT * FROM role WHERE role.name='ROLE_USER');
-
